1546c4349010019e65327051955f5f948b3811cd
valdezsantiago78 <svaldez@antel.com.uy>
2022-07-30 00:05:46 -0300

    websocket funcionando

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -28,9 +28,9 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    wsHost: 'ws.jano-ms-desa.in.iantel.com.uy',
-    wsPort: 80,    
-    encrypted: true,
+    wsHost: '127.0.0.1',
+    wsPort: 6001,    
+    encrypted: false,
     disableStats: false,
     forceTLS: false,
 
